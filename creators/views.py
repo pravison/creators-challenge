acd67e0978061ae4dba4e755980c5540149be191
pravison <pravisonjuma087@gmail.com>
2025-02-25 00:16:37 +0300

    working on whatsapp setup

--- creators/views.py
+++ creators/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from django.conf import settings 
+from django.http import HttpResponse
+from django.http import JsonResponse
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib import messages
