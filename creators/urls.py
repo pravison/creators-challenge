1206d9440e3bf30dc8e033779d5d9bc6f0a055a3
pravison <pravisonjuma087@gmail.com>
2025-02-25 00:01:38 +0300

    commit

--- creators/urls.py
+++ creators/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('add_creator/', views.add_creator, name='add_creator'),
+    path('02a10962-b38b-4d7a-a62c-4f22e2a32e46/', views.whatsappWebhook, name='whatsapp-webhook'),
     ]
